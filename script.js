f07e84318bf1affd897dfc884bf1f55a26437e26
Maximiliano Henríquez San Martín <maxi.henriquezsm@gmail.com>
2024-08-21 19:07:53 -0400

    Creación

---
+++ script.js
@@ -0,0 +1,60 @@
+const textArea = document.querySelector(".text__area");
+const mensaje = document.querySelector(".mensaje");
+const copiarBtn = document.querySelector(".copiar");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+
+    }
+    return stringEncriptada
+}   
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+
+    }
+    return stringDesencriptada
+}   
+
+function copiarAlPortapapeles() {
+    if (mensaje.value !== "") {
+        navigator.clipboard.writeText(mensaje.value).then(() => {
+            console.log("Texto copiado al portapapeles.");
+        }).catch(err => {
+            console.error("Error al copiar el texto: ", err);
+        });
+    } else {
+        console.log("No hay texto para copiar.");
+    }
+}
